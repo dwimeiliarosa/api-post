a5a4910f378c5c40745b1d6bbfa8601122bd4a05
dwimeiliarosa <chaavvv01@gmail.com>
2026-02-24 14:25:22 +0700

    Update code index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { swaggerUi, swaggerSpec } = require('./config/swagger');
 
 const authRoutes = require('./routes/authRoutes');
 const postRoutes = require('./routes/postRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
+
 
 const app = express();
 
@@ -14,9 +16,17 @@ app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use(authRoutes);
-app.use(postRoutes);
+app.use('/api', authRoutes);
+app.use('/api', postRoutes);
+app.use('/api', categoryRoutes);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    status: 'error',
+    message: err.message
+  });
+});
 app.listen(3000, () => {
   console.log('Server jalan di http://localhost:3000');
   console.log('Swagger di http://localhost:3000/api-docs');
